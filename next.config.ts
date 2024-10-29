4670350608e62ddd50694e474bfa585d51351d96
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2024-10-29 13:09:50 +0530

    Startup details completed

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,16 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {protocol: 'https',hostname: '*'}
     ]
-  }
+  },
+  experimental: {
+    ppr: "incremental",
+    after: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
 };
 
 export default nextConfig;
