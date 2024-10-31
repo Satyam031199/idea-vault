d271766056c69f2b4fb2debfb6b50bf2fbc6100c
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2024-10-31 13:47:11 +0530

    add user profile page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
-import { auth, signIn, signOut } from "@/auth"
-import Image from "next/image"
-import Link from "next/link"
+import { auth, signIn, signOut } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
   const session = await auth();
@@ -13,27 +15,45 @@ const Navbar = async () => {
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
-              <Link href="/startup/create"><span>Create</span></Link>
-              <form action={ async () => {
-              'use server'
-              await signOut({redirectTo: "/"})
-            }}>
-              <button type="submit">Logout</button>
-            </form>
-              <Link href={`/user/${session?.id}`}><span>{session?.user.name}</span></Link>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
+              </form>
+              <Link href={`/user/${session?.id}`}>
+              <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
+              </Link>
             </>
           ) : (
-            <form action={ async () => {
-              'use server'
-              await signIn('github')
-            }}>
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
               <button type="submit">Login</button>
             </form>
           )}
         </div>
-        </nav>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,9 +1,10 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
 
 export type StartupCardType = Omit<Startup,"author"> & {author?: Author};
 
@@ -38,7 +39,7 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
         </div>
         <Link href={`/user/${author?._id}`}>
           <Image
-            src="https://placehold.co/48x48"
+            src={author?.image!}
             alt={author?.name!}
             width={48}
             height={48}
@@ -63,5 +64,15 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
     </li>
   );
 };
+
+export const StartupCardSkeleton = () => (
+  <>
+    {[0, 1, 2, 3, 4].map((index: number) => (
+      <li key={cn("skeleton", index)}>
+        <Skeleton className="startup-card_skeleton" />
+      </li>
+    ))}
+  </>
+);
 
 export default StartupCard;
